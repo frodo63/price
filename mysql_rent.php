4e22000c70967ed48931607c2ae8ca0e019a2db8
frodo63 <frodo63@mail.ru>
2018-08-17 12:00:59 +0400

    Fixed rent.php not needing all pos to have winners. Counts of the present winners

--- mysql_rent.php
+++ mysql_rent.php
@@ -90,7 +90,7 @@ if (isset($_POST['request'])){
     $wincount = 0;
     $getvars = $pdo->prepare("SELECT `req_positionid`,`winnerid` FROM req_positions WHERE `req_positions`.`requestid` = ?");
     $nam == 0;//Переменная, в зависимости от того, зафиксирована расценка или нет
-    $countrent = $pdo->prepare("SELECT * FROM (SELECT `winnerid` FROM `req_positions` WHERE `requestid` = ?) AS a LEFT JOIN (SELECT `kol`,`price`,`rop`,`opr`,`fixed`,`wtime`,`pricingid`,`name`,`tradeid` FROM `pricings` AS c LEFT JOIN (SELECT `trades_id`,`name` FROM `trades` LEFT JOIN `allnames` ON `trades`.`trades_nameid`=`allnames`.`nameid`) AS d ON c.`tradeid`=d.`trades_id`) AS b ON a.`winnerid` = b.`pricingid`");
+    $countrent = $pdo->prepare("SELECT * FROM (SELECT `winnerid` FROM `req_positions` WHERE `requestid` = ? AND `winnerid` != 0 ) AS a LEFT JOIN (SELECT `kol`,`price`,`rop`,`opr`,`fixed`,`wtime`,`pricingid`,`name`,`tradeid` FROM `pricings` AS c LEFT JOIN (SELECT `trades_id`,`name` FROM `trades` LEFT JOIN `allnames` ON `trades`.`trades_nameid`=`allnames`.`nameid`) AS d ON c.`tradeid`=d.`trades_id`) AS b ON a.`winnerid` = b.`pricingid`");
     $save_rent = $pdo->prepare("UPDATE `requests` SET `req_rent`=? WHERE `requests_id`=?");
     $save_sum = $pdo->prepare("UPDATE `requests` SET `req_sum`=? WHERE `requests_id`=?");
     try{
@@ -100,7 +100,10 @@ if (isset($_POST['request'])){
         $pdo->commit();
 
         /*Число рядов в массиве покажет количество позиций*/
-        $poscount = $getvars->rowCount();
+
+        /*Убрал подсчет общй позиций*/
+        //$poscount = $getvars->rowCount();
+
         /*Число победителей считается перебором и сравнением с 0*/
         $p=$getvars->fetchAll(PDO::FETCH_ASSOC);
         foreach ($p as $row){
@@ -110,6 +113,7 @@ if (isset($_POST['request'])){
         }
 
         $c=$countrent->fetchAll(PDO::FETCH_ASSOC);
+        $c_count = $countrent->rowCount();
 
         $result="
                 <table class='rent-table'>
@@ -167,9 +171,11 @@ if (isset($_POST['request'])){
         };
         $result .="</table><br>";
 
-        $countdif = round($poscount,0) - round($wincount,0);
+        //Убрал расчет разницы, заменил на сравнение rowCount($c)  с 0
+        //$countdif = round($poscount,0) - round($wincount,0);
 
-        if ($countdif > 0){
+
+        if ($c_count == 0){
             /*Посылаем ноль в графу общей рентабельности*/
             $pdo->beginTransaction();
             $save_rent->execute(array(0, $reqid));
@@ -181,26 +187,22 @@ if (isset($_POST['request'])){
             $pdo->commit();
 
             print (json_encode(array(
-                "data1"=>"<span>Расчет общей суммы и рентабельности невозможен.<br><br>a)выберите победителя в каждой из позиций<br>b)удалите ненужные позиции.</span>
-            <br><br><span>Разница: " . ((int)$poscount - (int)$wincount) . " Позиций: " . $poscount . " Победителей: " . $wincount . "</span>",
+                "data1"=>"<span>Расчет общей суммы и рентабельности невозможен.<br><br>Назначьте хотя бы одного победителя.</span><br>",
                 "data2"=>"0.00",
                 "data3"=>"0.00")));
 
         };
-        if ($countdif == 0){
+        if ($c_count > 0){
             $top;
             $bot;
-            foreach($form_top as $key=>$value){$top = $top + $value;}
-            foreach($form_bot as $key=>$value){$bot = $bot + $value;}
+
+            /*расчет рентабельности*/
+            $top = round(array_sum($form_top), 3);
+            $bot = round(array_sum($form_bot), 3);
+            $rent = round($top/$bot*100, 2);
 
             $dem_top = substr($dem_top, 0, -3);//Срезаем лишние плюсы с демонстрационных строк
             $dem_bot = substr($dem_bot, 0, -3);
-
-            /*расчет рентабельности*/
-            $top = round($top, 3);
-            $bot = round($bot, 3);
-
-            $rent = round($top/$bot*100, 2);
 
             //Сохраняем в базу рентабельность заказа
             $pdo->beginTransaction();
@@ -211,7 +213,7 @@ if (isset($_POST['request'])){
             $pdo->beginTransaction();
             $save_sum->execute(array($bot, $reqid));
             $pdo->commit();
-            print (json_encode(array("data1"=>"Расчет рентабельности: <br><br><table class ='demo'><tr><td>" . $dem_top . "</td></tr><tr><td>" . $dem_bot . "</td></tr></table><br><br><span> Общая рентабельность: " . $rent . " % </span> " . $result . " <br><br><span>Разница: " . ((int)$poscount - (int)$wincount) . " Позиций: " . (int)$poscount . " Победителей: " . (int)$wincount . "</span>","data2"=>$rent,"data3"=>number_format($bot,2,'.',' ')." руб.")));
+            print (json_encode(array("data1"=>"Расчет рентабельности: <br><br><table class ='demo'><tr><td>" . $dem_top . "</td></tr><tr><td>" . $dem_bot . "</td></tr></table><br><br><span> Общая рентабельность: " . $rent . " % </span> " . $result . " <br><br>","data2"=>$rent,"data3"=>number_format($bot,2,'.',' ')." руб.")));
         };
 
     } catch( PDOException $Exception ) {
